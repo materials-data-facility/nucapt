81194665682f2c7b78fd93ee0771f309a4993e3a
Logan Ward <ward.logan.t@gmail.com>
2017-07-13 09:51:47 -0500

    Added abstract to dataset information

--- nucapt/forms.py
+++ nucapt/forms.py
@@ -1,4 +1,4 @@
-from wtforms import Form, StringField, FieldList, FormField
+from wtforms import Form, StringField, TextAreaField, FieldList, FormField
 
 
 class AuthorForm(Form):
@@ -11,6 +11,7 @@ class AuthorForm(Form):
 class CreateForm(Form):
     """Form for creating a new dataset"""
     title = StringField('Title')
+    abstract = TextAreaField('Abstract', description='Abstract describing this experiment. As long as necessary')
     authors = FieldList(FormField(AuthorForm), 'Authors',
                         min_entries=1,
                         description='People associated with this dataset')
