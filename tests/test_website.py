ffeb55ea215bf9b0491c4c9eef6e7f467e6c6f84
Logan Ward <ward.logan.t@gmail.com>
2017-08-11 18:01:12 -0500

    Added more tests for sample-related operations

--- tests/test_website.py
+++ tests/test_website.py
@@ -123,20 +123,11 @@ class TestWebsite(unittest.TestCase):
         self.assertEquals(200, rv.status_code)
 
         soup = BeautifulSoup(rv.data, 'html.parser')
-        name_field = soup.find('input', {'id':'sample_name'})
+        name_field = soup.find('input', {'id': 'sample_name'})
         self.assertEquals('Sample1', name_field['value'])
 
         # Create a new sample
-        data = {
-            'sample_name': 'Sample1',
-            'sample_form-sample_title': 'Example Sample',
-            'sample_form-sample_abstract': 'Sample description',
-            'sample_form-metadata-0-key': 'Aging time',
-            'sample_form-metadata-0-value': '2 hours',
-            'collection_form-evaporation_mode': 'laser',
-            'rhit_file':(BytesIO(b'My RHIT file contents'), 'EXAMPLE.RHIT')
-        }
-        rv = self.app.post('/dataset/%s/sample/create'%dataset_name, data=data)
+        data, rv = self.create_sample(dataset_name)
         self.assertEquals(302, rv.status_code)
 
         self.assertTrue(os.path.isdir(os.path.join(manager.data_path, dataset_name, 'Sample1')))
@@ -147,7 +138,81 @@ class TestWebsite(unittest.TestCase):
         self.assertEquals('Example Sample', sample_metadata['sample_title'])
         self.assertEquals('2 hours', sample_metadata['metadata'][0]['value'])
 
+        # Check that the file appeared
+        self.assertTrue(os.path.exists(os.path.join(manager.data_path, dataset_name, 'Sample1', 'EXAMPLE.RHIT')))
 
+        # Render the sample
+        rv = self.app.get('/dataset/%s/sample/Sample1'%dataset_name)
+        self.assertEquals(rv.status_code, 200)
+
+        self.assertTrue(b'No reconstructions' in rv.data)
+
+        # Make sure creating a second sample fails
+        data, rv = self.create_sample(dataset_name, 'Sample1')
+        self.assertEquals(200, rv.status_code)
+
+        self.assertTrue('Sample Sample1 already exists for dataset %s'%(dataset_name) in str(rv.data))
+
+        # Make a second sample
+        data, rv = self.create_sample(dataset_name, 'Sample2')
+        self.assertEquals(302, rv.status_code)
+
+        # Make sure both appear in dataset page
+        rv = self.app.get('/dataset/%s'%dataset_name)
+        self.assertIn(b'Sample1', rv.data)
+        self.assertIn(b'Sample2', rv.data)
+
+        # Edit the general metdata of Sample1
+        rv = self.app.get('/dataset/%s/sample/%s/edit_info'%(dataset_name, 'Sample1'))
+        self.assertEquals(200, rv.status_code)
+
+        soup = BeautifulSoup(rv.data, 'html.parser')
+        field = soup.find('input', {'id': 'sample_title'})
+        self.assertEquals('Example Sample', field['value'])
+
+        field = soup.find('input', {'id': 'metadata-0-key'})
+        self.assertEquals('Aging time', field['value'])
+
+        data = {
+            'sample_title': 'new title',
+            'sample_description': 'new description'
+        }
+        rv = self.app.post('/dataset/%s/sample/%s/edit_info'%(dataset_name, 'Sample1'), data=data)
+        self.assertEquals(302, rv.status_code)
+        sample = APTSampleDirectory.load_dataset_by_name(dataset_name, 'Sample1')
+        sample_metadata = sample.load_sample_information()
+        self.assertEquals('new title', sample_metadata['sample_title'])
+
+        # Edit the collection information
+        sample_metadata = sample.load_collection_metadata()
+
+        rv = self.app.get('/dataset/%s/sample/%s/edit_collection' % (dataset_name, 'Sample1'))
+        self.assertEquals(200, rv.status_code)
+
+        soup = BeautifulSoup(rv.data, 'html.parser')
+        field = soup.find('input', {'id': 'leap_model'})
+        self.assertEquals('A nice one', field['value'])
+
+        sample_metadata.metadata['leap_model'] = 'A super one'
+        rv = self.app.post('/dataset/%s/sample/%s/edit_collection' % (dataset_name, 'Sample1'),
+                           data=sample_metadata.metadata)
+        self.assertEquals(302, rv.status_code)
+        sample_metadata = sample.load_collection_metadata()
+        self.assertEquals('A super one', sample_metadata['leap_model'])
+
+    def create_sample(self, dataset_name, sample_name='Sample1'):
+        data = {
+            'sample_name': sample_name,
+            'sample_form-sample_title': 'Example Sample',
+            'sample_form-sample_abstract': 'Sample description',
+            'sample_form-metadata-0-key': 'Aging time',
+            'sample_form-metadata-0-value': '2 hours',
+            'collection_form-leap_model': 'A nice one',
+            'collection_form-evaporation_mode': 'laser',
+            'rhit_file': (BytesIO(b'My RHIT file contents'), 'EXAMPLE.RHIT')
+        }
+        rv = self.app.post('/dataset/%s/sample/create' % dataset_name, data=data)
+        return data, rv
 
 
 if __name__ == '__main__':
